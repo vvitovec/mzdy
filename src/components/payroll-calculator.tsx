cbdf29e6c7965fb2778d565bbc876db312ad4585
Viktor Vitovec <vvitovec27@gmail.com>
2026-05-05 01:07:34 +0200

    one line boss

--- src/components/payroll-calculator.tsx
+++ src/components/payroll-calculator.tsx
@@ -235,10 +235,7 @@ export function PayrollCalculator() {
       <section className="content">
         <header className="page-heading">
           <div>
-            <h1 className="hero-title">
-              <span>Mzdová kalkulačka</span>
-              <span>2026</span>
-            </h1>
+            <h1>Mzdová kalkulačka 2026</h1>
           </div>
         </header>
 
